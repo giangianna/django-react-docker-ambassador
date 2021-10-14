a77f64939d2e8a0ca7ec2064545a91e0caa78041
Gian Gianna <gian.ule@gmail.com>
2021-10-14 09:47:31 +0700

    django admin API Login

--- common/views.py
+++ common/views.py
@@ -2,6 +2,7 @@ from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.models import User
 from .serilizers import UserSerializer
 
 
@@ -19,3 +20,18 @@ class RegisterAPIView(APIView):
         serializers.is_valid(raise_exception=True)
         serializers.save()
         return Response(serializers.data)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect Password!')
+        
+        return Response(UserSerializer(user).data)
