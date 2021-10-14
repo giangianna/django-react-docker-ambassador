fb6e4e8df85aad79c5c42bc171992faef7deadb5
Gian Gianna <giangianna@Gians-MacBook-Air.local>
2021-10-14 09:32:57 +0700

    django admin register

--- common/views.py
+++ common/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .serilizers import UserSerializer
 
 
 # Create your views here.
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response('hello')
+        data = request.data
+
+        if data['password'] != data['password_confrim']:
+            raise exceptions.APIException('Password do not match!')
+        
+        data['is_ambassador'] = 0
+
+        serializers = UserSerializer(data=data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data)
