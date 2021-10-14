ff069ac9415d6680afc828cbf892889e9efa6ae8
Gian Gianna <giangianna@Gians-MacBook-Air.local>
2021-10-14 09:01:07 +0700

    first commit

---
+++ common/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, request):
+        return Response('hello')
