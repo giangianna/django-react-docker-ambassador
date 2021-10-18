1680a84d9173dfc3b5f3aa1bc000b865d5482ed2
Gian Gianna <gian.ule@gmail.com>
2021-10-18 19:44:34 +0700

    django ambassador create product enpoind

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import ProductSerializer
+from core.models import Product
 
 # Create your views here.
+class ProductFrontendAPIView(APIView):
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductBackendAPIView(APIView):
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
